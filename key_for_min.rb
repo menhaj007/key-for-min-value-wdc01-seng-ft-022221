825b1342974b0309640a3a651e8f7ef2503d60bb
witty-trace-1906 <menhajuddin.sharaf@gmail.com>
2020-12-28 19:59:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_key = name_hash[0]
+  min_value = name_hash[1]
+  
+  name_hash.each do |key, value|
+    if ()
+  end
 end
