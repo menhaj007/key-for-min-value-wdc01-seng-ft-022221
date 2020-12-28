b6ab5ea458f27c2e97099f4c084bb3a861ed29c2
witty-trace-1906 <menhajuddin.sharaf@gmail.com>
2020-12-28 20:11:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,11 +1,11 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-  min_key = name_hash[0]
-  min_value = name_hash[1]
+# def key_for_min_value(name_hash)
+#   min_key = name_hash[0]
+#   min_value = name_hash[1]
   
-  name_hash.each do |key, value|
-    if ()
-  end
-end
+#   name_hash.each do |key, value|
+#     if ()
+#   end
+# end
