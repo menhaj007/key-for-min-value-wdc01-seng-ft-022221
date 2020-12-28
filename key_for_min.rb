ed99cb7ee73a4ea8b402de6cb1f1f411f72b5cd5
witty-trace-1906 <menhajuddin.sharaf@gmail.com>
2020-12-28 20:11:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,3 +9,22 @@
 #     if ()
 #   end
 # end
+
+def key_for_min_value(name_hash)
+    if (name_hash.empty?)
+        return nil
+    end
+    min_value = name_hash.first[1] #-2
+    min_key = name_hash.first[0]
+
+    # puts "The key is: #{min_key}"
+    # puts "The value is: #{min_value}"
+    name_hash.each do |key, value|
+        if (value < min_value)
+            min_value = value
+            min_key = key
+        end
+    end
+    puts min_key
+    puts min_value
+end
